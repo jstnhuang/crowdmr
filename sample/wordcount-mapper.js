a73ac2ee13d892f464fcb6a60951f1888d69b373
Justin Huang <jstn@cs.washington.edu>
2014-03-09 23:01:44 -0700

    Updated mapper to combine results.

--- sample/wordcount-mapper.js
+++ sample/wordcount-mapper.js
@@ -1,16 +1,30 @@
-var result = [];
-for (var i in lines) {  
+var results = [];
+var allWords = [];
+for (var i=0; i<lines.length; i++) {  
   if (i % 10000 === 0) {
     console.log(i);
   }
   var line = lines[i];
   var words = line.split(/\s+/);
-  for (var j in words) {
+  for (var j=0; j<words.length; j++) {
     var word = words[j];
     if (word === '') {
       continue;
     }
-    result.push([word, '1'].join('\t')); 
+    allWords.push(word);
   }
 }
-return result;
+allWords.sort();
+var prevWord = allWords[0];
+var count = 1;
+for (var i=1; i<allWords.length; i++) {
+  var word = allWords[i];
+  if (word !== prevWord) {
+    results.push([prevWord, count].join('\t'));
+    prevWord = word;
+    count = 1;
+  } else {
+    count++;
+  }
+}
+return results;
